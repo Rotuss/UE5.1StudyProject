8d55de03e7d104b5ed642af9aaa34ee1e8494c5f
Rotus <12rotus31@gmail.com>
2024-06-28 15:01:56 +0900

    Rotus: Server, PC Test
    - Server, Owner, Other 인식에 대한 로그 확인

--- Source/StudyProject/Private/WorldStatic/SLandMine.cpp
+++ Source/StudyProject/Private/WorldStatic/SLandMine.cpp
@@ -3,6 +3,7 @@
 
 #include "WorldStatic/SLandMine.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
 
@@ -28,7 +29,51 @@ void ASLandMine::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called.")));
+	//UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called.")));
+
+    // HasAuthority(): 반환값이 true이면 서버에서 실행 중, false이면 서버가 아닌 PC에서 실행 중
+    if (true == HasAuthority())
+    {
+        UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in Server PC.")));
+    }
+    // 즉 해당 else 부분은 서버가 아닌 PC일 때의 조건
+    else
+    {
+        // 내 컴퓨터(Owning)
+        if (GetOwner() == UGameplayStatics::GetPlayerController(this, 0))
+        {
+            UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in OwningClient PC.")));
+        }
+        // 내 컴퓨터가 아닌 다른 컴퓨터(Other)
+        else
+        {
+            UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in OtherClient PC.")));
+        }
+    }
+
+}
+
+void ASLandMine::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	//UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called.")));
+
+    if (true == HasAuthority())
+    {
+        UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in Server PC.")));
+    }
+    else
+    {
+        if (GetOwner() == UGameplayStatics::GetPlayerController(this, 0))
+        {
+            UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in OwningClient PC.")));
+        }
+        else
+        {
+            UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in OtherClient PC.")));
+        }
+    }
 
 }
 
